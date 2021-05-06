bd76c975b14b2cbefac1cdd32e4aebe20dc6adb4
bogdanov <iceposhta@gmail.com>
2021-05-06 14:55:08 +0100

    add a TipPercentages picker

--- Tip calculator/ContentView.swift
+++ Tip calculator/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ContentView: View {
     @State private var checkAmount = ""
     @State private var numberOfPeople = 2
+    @State private var tipPercentage = 0
+    let tipPercentages = [0, 5, 10, 15, 20]
 
     var body: some View {
         NavigationView {
@@ -21,6 +23,14 @@ struct ContentView: View {
                             Text("\($0) people")
                         }
                     }
+                }
+                Section(header: Text("Desired amount of tips")) {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
                 }
             }
             .navigationBarTitle("Tip Calculator", displayMode: .inline)
