13930823986f8db3271b4ef9eb648614a4657904
bogdanov <iceposhta@gmail.com>
2021-05-06 14:24:31 +0100

    Initial Commit

---
+++ Tip calculator/Tip_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tip_calculatorApp.swift
+//  Tip calculator
+//
+//  Created by bogdanov on 06.05.21.
+//
+
+import SwiftUI
+
+@main
+struct Tip_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
